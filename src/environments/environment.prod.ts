2f8566a476641262584c5270b78d3d63d096a49a
vates_abiss <vates_abiss@lnexternos.com.ar>
2020-11-30 23:18:27 -0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://bi-qr-menu-app.herokuapp.com',
+  publicKey: 'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik51ZXZvIFByb2dyZXNvIENhZmUiLCJpYXQiOjE1MTYyMzkwMjJ9',
+  pathGit: '/qr-menu-app/#/',
+  files: {
+    pdf: 10,
+    image: 10
+  }
+};
